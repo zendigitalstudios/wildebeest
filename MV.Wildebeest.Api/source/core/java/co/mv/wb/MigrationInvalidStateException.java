8b0dd603bd6575dc51e7e0cfdd2be741d78fdbbc
Tomislav Jurin <svezauzeto12@hotmail.com>
2018-06-12 23:07:34 +0200

    MVWB-11 added new MigrationException and added function to validate migration states in WildebeestApiImpl

--- MV.Wildebeest.Api/source/core/java/co/mv/wb/MigrationInvalidStateException.java
+++ MV.Wildebeest.Api/source/core/java/co/mv/wb/MigrationInvalidStateException.java
@@ -9,6 +9,8 @@ import java.util.UUID;
  */
 public class MigrationInvalidStateException extends Exception
 {
+	private UUID migrationId = null;
+	private boolean migrationIdSet = false;
 	/**
 	 * Creates a new MigrationFailedException for the specified ID with the specified failure messages.
 	 *
@@ -16,10 +18,37 @@ public class MigrationInvalidStateException extends Exception
 	 * @since                                   1.0
 	 */
 	public MigrationInvalidStateException(
+		  UUID migrationId,
 		  String message)
 	{
 		super(message);
+		this.setMigrationId(migrationId);
 
+	}
+
+	/**
+	 * Gets the ID of the Migration that failed
+	 *
+	 * @return                                  the ID of the Migration that failed
+	 * @since                                   1.0
+	 */
+	public UUID getMigrationId() {
+		if(!migrationIdSet) {
+			throw new IllegalStateException("migrationId not set.  Use the HasMigrationId() method to check its state before accessing it.");
+		}
+		return migrationId;
+	}
+
+	private void setMigrationId(
+		  UUID value) {
+		if(value == null) {
+			throw new IllegalArgumentException("migrationId cannot be null");
+		}
+		boolean changing = !migrationIdSet || migrationId != value;
+		if(changing) {
+			migrationIdSet = true;
+			migrationId = value;
+		}
 	}
 
 }
